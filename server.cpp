8e1ebdd201eccae2228d382fdc289125598ad582
Alexandr Smirnov <buyer@mail.ru>
2016-03-01 14:00:24 +0500

    added Cmake

--- server.cpp
+++ server.cpp
@@ -65,9 +65,36 @@ int main(int argc, char **argv) {
 	//TODO: разбор командной строки
 	//--------------------------------
 	//parce_cmd_line(argc, argv, ip_str, port_str, root_directory);
+	
 	string ip_str = "127.0.0.1";
 	string port_str = "12345";
 	string root_directory = "sites";
+	
+	static const char *opts = "h:p:d:"; // доступные опции, каждая принимает аргумент
+	int opt; // каждая следующая опция попадает сюда
+	while((opt = getopt(argc, argv, opts)) != -1) { // вызываем getopt пока она не вернет -1
+		switch(opt) {
+			case 'h': // если опция -h возвращаем строку
+				ip_str = optarg;
+				break;
+			case 'p': // если опция -p, преобразуем строку с аргументом в число
+				port_str = optarg;
+				break;
+			case 'd': //  если опция -d возвращаем строку
+				root_directory = optarg;
+				break;
+		}
+	}
+
+	//cout << "ip_str: " << ip_str << endl;
+	//cout << "port_str: " << port_str << endl;
+	//cout << "root_directory: " << root_directory << endl;
+	//exit(0);
+
+	//ip_str = "127.0.0.1";
+	//port_str = "12345";
+	//root_directory = "sites";
+
 	if( root_directory.back() == '/' )
 		root_directory = root_directory.substr(0, (root_directory.length() - 1));
 
@@ -127,14 +154,14 @@ int main(int argc, char **argv) {
 
 	CHK2( MasterSocket , socket(PF_INET, SOCK_STREAM, IPPROTO_TCP) );
 	DBG( printf("master: Main listener(fd=%d) created! \n", MasterSocket); )
-	
-	int optval = 1;
+
+		int optval = 1;
 	setsockopt(MasterSocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-	
+
 	CHK( bind(MasterSocket, (struct sockaddr *) (&SockAddr), sizeof(SockAddr)) );
 	DBG( printf("master: Listener binded to: %s\n", port_str.c_str()); )
-	
-	CHK( listen(MasterSocket, SOMAXCONN) );
+
+		CHK( listen(MasterSocket, SOMAXCONN) );
 
 	int conn_sock;
 	//event loop for listen socket
@@ -142,7 +169,7 @@ int main(int argc, char **argv) {
 		// принимаем сокет и передаем дескриптор worker'у
 		CHK2( conn_sock, accept(MasterSocket, NULL, NULL));
 		DBG( printf("master: New client (fd = %d)\n", conn_sock);)
-		set_nonblock(conn_sock);
+			set_nonblock(conn_sock);
 		char ch[] = "1";
 		//---------------------------
 		// Выбор потомков для работы
@@ -167,15 +194,15 @@ void child_event_loop(int children_i, int fd_ch2parent, string root_directory) {
 	//size_t max_num_request = 1000;
 	//char *buf_request;
 	static const char* templ = "HTTP/1.0 200 OK\r\n"
-					           "Content-length: %d\r\n"
-							   "Content-Type: text/html\r\n"
-							   "\r\n";
+		"Content-length: %d\r\n"
+		"Content-Type: text/html\r\n"
+		"\r\n";
 
 
 	static const char not_found_header[] = "HTTP/1.0 404 NOT FOUND\r\n"
-										   "Content-length: %d\r\n"
-										   "Content-Type: text/html\r\n"
-										   "\r\n";
+		"Content-length: %d\r\n"
+		"Content-Type: text/html\r\n"
+		"\r\n";
 	static const char not_found_msg[]    = "<h1>404 - File not found.</h1>\r\n";
 
 	map<int, string> str_request; // num_socket:str_request;
@@ -202,84 +229,84 @@ void child_event_loop(int children_i, int fd_ch2parent, string root_directory) {
 	while(true) {
 		CHK2( epoll_events_count, epoll_wait(epfd, Events, MAX_EVENTS,-1));
 		DBG( printf("children %d: Epoll events count: %d\n", children_i, epoll_events_count);)
-		DBG( printf("children %d: PID: %d\n", children_i, getpid());)
-		for(i = 0; i < epoll_events_count ; ++i) {
-			int currfd = Events[i].data.fd;
-			// Сообщение с канала от родителя - принимаем дескриптор нового соединения
-			if (Events[i].data.fd == fd_ch2parent) {
-				size = sock_fd_read(fd_ch2parent, buf, sizeof(buf), &conn_sock);
-				if (size <= 0)
-					break;
-				DBG( printf("children %d: read %d byte (fd passing)\n", children_i, size);)
-				DBG( printf("children %d: New client (fd = %d)\n", children_i,  conn_sock);)
-				Event.events = EPOLLIN;// | EPOLLET;
-				Event.data.fd = conn_sock;
-				CHK( epoll_ctl(epfd, EPOLL_CTL_ADD, conn_sock,	&Event) == -1) 
-				DBG( printf("children %d: Added new client (fd = %d) to epoll\n", children_i,  conn_sock);)
-				//write(Events[i].data.fd, "#Hello, client!\n", 16);
-			} else { //событие от потомка
-				DBG(printf("children %d: Try to read from fd(%d)\n", children_i, currfd);)
-				int len = recv(currfd, buf, BUF_SIZE, 0);
-				if( len == 0) {
-					close(currfd);
-					break;
-				}
+			DBG( printf("children %d: PID: %d\n", children_i, getpid());)
+			for(i = 0; i < epoll_events_count ; ++i) {
+				int currfd = Events[i].data.fd;
+				// Сообщение с канала от родителя - принимаем дескриптор нового соединения
+				if (Events[i].data.fd == fd_ch2parent) {
+					size = sock_fd_read(fd_ch2parent, buf, sizeof(buf), &conn_sock);
+					if (size <= 0)
+						break;
+					DBG( printf("children %d: read %d byte (fd passing)\n", children_i, size);)
+						DBG( printf("children %d: New client (fd = %d)\n", children_i,  conn_sock);)
+						Event.events = EPOLLIN;// | EPOLLET;
+					Event.data.fd = conn_sock;
+					CHK( epoll_ctl(epfd, EPOLL_CTL_ADD, conn_sock,	&Event) == -1) 
+						DBG( printf("children %d: Added new client (fd = %d) to epoll\n", children_i,  conn_sock);)
+						//write(Events[i].data.fd, "#Hello, client!\n", 16);
+				} else { //событие от потомка
+					DBG(printf("children %d: Try to read from fd(%d)\n", children_i, currfd);)
+						int len = recv(currfd, buf, BUF_SIZE, 0);
+					if( len == 0) {
+						close(currfd);
+						break;
+					}
 
-				string chunk = string(buf, len);
-				//Оптимизации по работе со строками по добавлению новых chunks в запрос нет, не продакшн
-				//printf("\tПолучен chunk: %s\n",chunk.c_str());
-				//если запроса с таким дескриптором еще не было
-				if(str_request.find(currfd) == str_request.end()) {
-					//printf("\tНовый запрос\n");
-					str_request.insert(pair<int, string>(currfd,chunk));
-					//printf("\tВ буфере после добавления %d байт\n", str_request[currfd].length());
-				} else {
-					//printf("\tПовторный  запрос\n");
-					//printf("\tВ буфере перед добавлением %d байт\n", str_request[currfd].length());
-					str_request[currfd] += chunk;
-					//printf("\tВ буфере после добавления %d байт\n", str_request[currfd].length());
+					string chunk = string(buf, len);
+					//Оптимизации по работе со строками по добавлению новых chunks в запрос нет, не продакшн
+					//printf("\tПолучен chunk: %s\n",chunk.c_str());
+					//если запроса с таким дескриптором еще не было
+					if(str_request.find(currfd) == str_request.end()) {
+						//printf("\tНовый запрос\n");
+						str_request.insert(pair<int, string>(currfd,chunk));
+						//printf("\tВ буфере после добавления %d байт\n", str_request[currfd].length());
+					} else {
+						//printf("\tПовторный  запрос\n");
+						//printf("\tВ буфере перед добавлением %d байт\n", str_request[currfd].length());
+						str_request[currfd] += chunk;
+						//printf("\tВ буфере после добавления %d байт\n", str_request[currfd].length());
+					}
+					//write(STDOUT_FILENO, buf, len);
+					//write(STDOUT_FILENO, str_request[currfd].c_str(), str_request[currfd].length() );
+					if( (str_request[currfd].find("\n\n") == string::npos) && (str_request[currfd].find("\n\r\n") == string::npos))
+						continue;
+					//write(STDOUT_FILENO, str_request[currfd].c_str(), str_request[currfd].length() );
+
+					//-----------------------------------------------
+					// запрос полностью получен => парсим, отвечаем
+					//-----------------------------------------------
+					//write(currfd, buf, len);
+					map<string, string> headers;
+					string method, uri, http_ver;
+					parse_request(str_request[currfd], method, uri, http_ver);
+					string target_file;
+					int pos_param = uri.find("?");
+					if(pos_param != string::npos)
+						target_file = uri.substr(0, pos_param);
+					else
+						target_file = uri;
+					//cout << "Target file: " <<  target_file << endl;
+					string fullpath = root_directory + target_file;
+					FILE *fp = fopen(fullpath.c_str(), "r");
+					//cout << "\tFullpath: " << fullpath << endl;
+					//cout << "\tFILE: " << fp << endl;
+					if( fp == NULL || !is_regular_file(fullpath.c_str()) ) {
+						dprintf(currfd, not_found_header, sizeof(not_found_msg));
+						write(currfd, not_found_msg, sizeof(not_found_msg));
+					} else {
+						struct stat buf;
+						fstat(fileno(fp), &buf);
+						int file_size = buf.st_size;
+						dprintf(currfd, templ, file_size);
+						sendfile(currfd, fileno(fp), NULL, file_size);
+						fclose(fp);
+					}
+					//close(currfd);
+					shutdown(currfd, SHUT_RDWR);
+					str_request[currfd] = "";
+					//printf("===================================================");
 				}
-				//write(STDOUT_FILENO, buf, len);
-				//write(STDOUT_FILENO, str_request[currfd].c_str(), str_request[currfd].length() );
-				if( (str_request[currfd].find("\n\n") == string::npos) && (str_request[currfd].find("\n\r\n") == string::npos))
-					continue;
-				//write(STDOUT_FILENO, str_request[currfd].c_str(), str_request[currfd].length() );
-				
-				//-----------------------------------------------
-				// запрос полностью получен => парсим, отвечаем
-				//-----------------------------------------------
-				//write(currfd, buf, len);
-				map<string, string> headers;
-				string method, uri, http_ver;
-				parse_request(str_request[currfd], method, uri, http_ver);
-				string target_file;
-				int pos_param = uri.find("?");
-				if(pos_param != string::npos)
-					 target_file = uri.substr(0, pos_param);
-				else
-					 target_file = uri;
-				//cout << "Target file: " <<  target_file << endl;
-				string fullpath = root_directory + target_file;
-				FILE *fp = fopen(fullpath.c_str(), "r");
-				//cout << "\tFullpath: " << fullpath << endl;
-				//cout << "\tFILE: " << fp << endl;
-				if( fp == NULL || !is_regular_file(fullpath.c_str()) ) {
-					dprintf(currfd, not_found_header, sizeof(not_found_msg));
-					write(currfd, not_found_msg, sizeof(not_found_msg));
-				} else {
-					struct stat buf;
-					fstat(fileno(fp), &buf);
-					int file_size = buf.st_size;
-					dprintf(currfd, templ, file_size);
-					sendfile(currfd, fileno(fp), NULL, file_size);
-					fclose(fp);
-				}
-				//close(currfd);
-				shutdown(currfd, SHUT_RDWR);
-				str_request[currfd] = "";
-				//printf("===================================================");
 			}
-		}
 	}
 
 };
